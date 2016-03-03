0a716dfda6873d4df39f0ca8cc280c124c6dcae7
Jaskar <anthony.gee@live.fr>
2016-03-03 21:59:35 +0100

    Fix des requêtes de l'appli vers l'API. Il n'y a maintenant plus de problèmes de cache.

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -32,42 +32,48 @@ function ajax(query, category, param, onSuccess, onError) {
         }
     };
 
+    var xhttpType = "GET";
+    var xhttpLink = "";
+    var xhttpParam = null;
+
     switch(query) {
         case "GET/ALL" : // Get all the things from a category
-            xhttp.open("GET", "http://" + AJAX_IP + "/" + category, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.setRequestHeader("token", AJAX_TOKEN);
-            xhttp.send();
+            xhttpType = "GET";
+            xhttpLink = "http://" + AJAX_IP + "/" + category;
             break;
         case "GET/ID": // Get an item in a category by its ID
-            xhttp.open("GET", "http://" + AJAX_IP + "/" + category + "/" + param, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.setRequestHeader("token", AJAX_TOKEN);
-            xhttp.send();
+            xhttpType = "GET";
+            xhttpLink = "http://" + AJAX_IP + "/" + category + "/" + param;
             break;
         case "GET/SEARCH": // Get all items with searched text in it
-            xhttp.open("GET", "http://" + AJAX_IP + "/" + category + "/search/" + param, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.setRequestHeader("token", AJAX_TOKEN);
-            xhttp.send();
+            xhttpType = "GET";
+            xhttpLink = "http://" + AJAX_IP + "/" + category + "/search/" + param;
             break;
 
         case "POST": // Add an item
-            xhttp.open("POST", "http://" + AJAX_IP + "/" + category + "/", true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.setRequestHeader("token", AJAX_TOKEN);
-            xhttp.send(param);
+            xhttpType = "POST";
+            xhttpLink = "http://" + AJAX_IP + "/" + category + "/";
+            xhttpParam = param;
             break;
 
         case "UPDATE": // Update an item
-            xhttp.open("POST", "http://" + AJAX_IP + "/" + category + "/update/", true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.setRequestHeader("token", AJAX_TOKEN);
-            xhttp.send(param);
+            xhttpType = "POST";
+            xhttpLink = "http://" + AJAX_IP + "/" + category + "/update/";
+            xhttpParam = param;
             break;
 
         default :
             onError('000', 'Mauvaise requête.');
             break;
     }
+
+    xhttp.open(xhttpType, xhttpLink, true);
+    xhttp.setRequestHeader("Cache-Control", "no-cache");
+    xhttp.setRequestHeader("Pragma", "no-cache");
+    xhttp.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.setRequestHeader("token", AJAX_TOKEN);
+    if(xhttpParam) xhttp.send(param);
+    else xhttp.send();
+
 }
